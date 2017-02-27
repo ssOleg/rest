3637ec5fbf1c1b316f979897fccd4c4f71055911
Oleh Babiy <obabiy@zenoss.com>
2017-02-27 22:24:53 +0200

    Add update and delete options and update urls

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.AllModules.as_view(), name='all_modules'),
+    url(r'^$', views.ListCreateModule.as_view(), name='all_modules'),
+    url(r'(?P<pk>\d+)/$', views.RetrieveUpdateDestroyModule.as_view(), name='rud_modules'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,28 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+# from django.shortcuts import render
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+# # Create your views here.
+
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
 
 
 from . import models
 from . import serializers
 
 
-class AllModules(APIView):
-    """docstring for ClassName."""
+class ListCreateModule(generics.ListCreateAPIView):
+    queryset = models.Module.objects.all()
+    serializer_class = serializers.ModuleSerializer
 
-    def get(self, request, format=None):
-        """Docstring for MethodName."""
-        module = models.Module.objects.all()
-        serializer = serializers.ModuleSerializer(module, many=True)
-        return Response(serializer.data)
 
-    def post(self, request, format=None):
-        """Docstring for MethodName."""
-        serializer = serializers.ModuleSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class RetrieveUpdateDestroyModule(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Module.objects.all()
+    serializer_class = serializers.ModuleSerializer
+
+
+# class AllModules(APIView):
+#     """docstring for ClassName."""
+
+#     def get(self, request, format=None):
+#         """Docstring for MethodName."""
+#         module = models.Module.objects.all()
+#         serializer = serializers.ModuleSerializer(module, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request, format=None):
+#         """Docstring for MethodName."""
+#         serializer = serializers.ModuleSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
